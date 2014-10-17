5bda13f5ffce2b7a4879cc7cca8d06db1faea816
Dizzy <zacepin@media-sfera.com>
2014-10-17 09:50:29 +0400

    update

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,14 +7,13 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-print_r($_SERVER);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1','fe80::1', '::1')) || php_sapi_name() === 'cli-server')
-    || in_array($_SERVER['REMOTE_ADDR'],array('84.17.27.174'))
+    || in_array($_SERVER['HTTP_X_REAL_IP'],array('84.17.27.174'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
